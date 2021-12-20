ab0e3c51b30f2c9b5ca6222652ef07ced3f3c00a
Wessel Kuijstermans <w.kuijstermans@home.nl>
2021-12-20 10:44:36 +0100

    Styled navbar to moodboard

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -4,12 +4,7 @@ import { Component, Input, OnInit } from '@angular/core'
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-
-  styles: [
-    '.btn-link { color: rgba(255,255,255,.5); text-decoration: none; }',
-    // tslint:disable-next-line: max-line-length
-    '.btn-link.focus, .btn-link:focus, .btn-link.hover, .btn-link:hover { color: rgba(255,255,255,.75); text-decoration: none; box-shadow: none; }'
-  ]
+  styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
   @Input() title: string = ''
