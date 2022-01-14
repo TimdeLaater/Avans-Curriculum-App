42d400f72d6aae5db09d1f77d7b4cba6b940211b
Enes Aygun <aygunenes535@gmail.com>
2022-01-14 15:25:09 +0100

    wegenbouwer implementation

--- src/app/module-choose/module-choose.component.ts
+++ src/app/module-choose/module-choose.component.ts
@@ -1,106 +1,138 @@
-import { Component, OnInit } from '@angular/core';
-import {CdkDrag, CdkDragDrop, DragDropModule, moveItemInArray, transferArrayItem} from '@angular/cdk/drag-drop';
+import { Component, DebugElement, OnInit } from '@angular/core';
+import {
+  CdkDrag,
+  CdkDragDrop,
+  moveItemInArray,
+  transferArrayItem,
+} from '@angular/cdk/drag-drop';
 import { Module } from 'src/models/module.model';
 import { ModuleService } from '../module.service';
+import { AlertService } from '../alert.service';
 
 @Component({
   selector: 'app-module-choose',
   templateUrl: './module-choose.component.html',
-  styleUrls: ['./module-choose.component.css']
+  styleUrls: ['./module-choose.component.css'],
 })
 export class ModuleChooseComponent implements OnInit {
-  modules$ : Module[] = [
+
+  modules$: Module[] = [
     {
-      _id: "1",
-      name: "Test Module 4",
-      detail: "Test",
-      subjects: [{
-        _id: "1",
-        academyCodes: ["1"],
-        name: "Subject1",
-        studyPoints: 1,
-        details: "Lorem bla bla bla",
-        environment: "online"
-      },
-    ]
+      _id: '1',
+      name: 'Test Module 4',
+      detail: 'Test',
+      subjects: [
+        {
+          _id: '1',
+          academyCodes: ['1'],
+          name: 'Subject1',
+          studyPoints: 1,
+          details: 'Lorem bla bla bla',
+          environment: 'online',
+        },
+      ],
+    },
+    {
+      _id: '1',
+      name: 'Test Module 5',
+      detail: 'Test',
+      subjects: [
+        {
+          _id: '1',
+          academyCodes: ['1'],
+          name: 'Subject1',
+          studyPoints: 1,
+          details: 'Lorem bla bla bla',
+          environment: 'online',
+        },
+      ],
     },
   ];
 
-  currentStreet$ : Module[] = [{
-    _id: "1",
-    name: "Test Module 1",
-    detail: "Test",
-    subjects: [{
-      _id: "1",
-      academyCodes: ["1"],
-      name: "Subject1",
-      studyPoints: 1,
-      details: "Lorem bla bla bla",
-      environment: "online"
+  currentStreet$: Module[] = [
+    {
+      _id: '1',
+      name: 'Test Module 1',
+      detail: 'Test',
+      subjects: [
+        {
+          _id: '1',
+          academyCodes: ['1'],
+          name: 'Subject1',
+          studyPoints: 1,
+          details: 'Lorem bla bla bla',
+          environment: 'online',
+        },
+      ],
     },
-  ]
-  },
-  {
-    _id: "1",
-    name: "Test Module 2",
-    detail: "Test",
-    subjects: [{
-      _id: "1",
-      academyCodes: ["1"],
-      name: "Subject1",
-      studyPoints: 1,
-      details: "Lorem bla bla bla",
-      environment: "online"
+    {
+      _id: '1',
+      name: 'Test Module 2',
+      detail: 'Test',
+      subjects: [
+        {
+          _id: '1',
+          academyCodes: ['1'],
+          name: 'Subject1',
+          studyPoints: 1,
+          details: 'Lorem bla bla bla',
+          environment: 'online',
+        },
+      ],
     },
-  ]
-  },
-  new Module(),
-  {
-    _id: "1",
-    name: "Test Module 3",
-    detail: "Test",
-    subjects: [{
-      _id: "1",
-      academyCodes: ["1"],
-      name: "Subject1",
-      studyPoints: 1,
-      details: "Lorem bla bla bla",
-      environment: "online"
+    new Module(),
+    {
+      _id: '1',
+      name: 'Test Module 3',
+      detail: 'Test',
+      subjects: [
+        {
+          _id: '1',
+          academyCodes: ['1'],
+          name: 'Subject1',
+          studyPoints: 1,
+          details: 'Lorem bla bla bla',
+          environment: 'online',
+        },
+      ],
     },
-  ]
-  }
-]
+  ];
 
-  constructor(private moduleService: ModuleService) { }
+  constructor(
+    private moduleService: ModuleService,
+    private alertService: AlertService
+  ) {}
 
   ngOnInit(): void {
-    this.getModules();
+    // this.getModules();
   }
 
-  drop(event: CdkDragDrop<Module[]>) {
-    if (event.previousContainer === event.container) {
-      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
-    } else {
-      transferArrayItem(
-        event.previousContainer.data,
-        event.container.data,
-        event.previousIndex,
-        event.currentIndex,
+  swap(event: CdkDragDrop<Module[]>) {
+    console.log('Swapping empty card with selected choice!');
+    // Get old target
+    let oldtarget = this.modules$[event.previousIndex];
+    if (this.currentStreet$[event.currentIndex + 2].name == '') {
+      console.log(
+        `The data from index ${
+          this.currentStreet$[event.currentIndex]
+        } is null!`
       );
+      this.modules$.splice(event.previousIndex, 1);
+    } else {
+      this.modules$[event.previousIndex] =
+        this.currentStreet$[event.currentIndex + 2];
     }
+    // give the selected card to the current street
+    this.currentStreet$[event.currentIndex + 2] = oldtarget;
   }
 
-  PredicateIndexEquals(index: CdkDrag<Module>){
-    return
+  getModules() {
+    this.moduleService.getAllModules().subscribe(
+      (data: any) => {
+        console.log(data);
+        this.modules$ = data;
+      },
+      (error) => console.log(error)
+    );
   }
-
-  getModules(){
-    this.moduleService.getAllModules().subscribe((data : any) => {
-      console.log(data);
-      this.modules$ = data;
-    }, 
-    (error) => console.log(error));
-  }
-
-
 }
--- src/models/module.model.ts
+++ src/models/module.model.ts
@@ -6,3 +6,4 @@ export class Module {
     detail: String = ''
     subjects: Subject[] = [];
 }
+
